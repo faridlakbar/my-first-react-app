0593dcf98057b8e0269e072d0c953622b60b3724
faridl akbarullah <akbar12ullah@gmail.com>
2024-01-31 14:23:20 +0700

    list property

---
+++ my-first-react-app/src/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card() {
+  return (
+    <div className="card">
+      <img src="https://via.placeholder.com/150" alt="profile picture" />
+      <h1>Faridl Mukhlashin Akbarullah</h1>
+      <p>saya sangat suka pemrograman</p>
+    </div>
+  );
+}
